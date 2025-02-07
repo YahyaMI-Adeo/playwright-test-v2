48f42e8a29c4b8bc2a1fbb8ac24f0a2ce388b90a
Valentin <valentintartare59@gmail.com>
2025-02-07 11:22:51 +0100

    fix: fix project structure

--- spt-supplier-test-e2e-main/dist/tests/helpers/environment.js
+++
@@ -1,5 +0,0 @@
-export var Environment;
-(function (Environment) {
-    Environment[Environment["INTERNAL"] = 0] = "INTERNAL";
-    Environment[Environment["EXTERNAL"] = 1] = "EXTERNAL";
-})(Environment || (Environment = {}));
